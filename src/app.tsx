74587a839a8e32440e63512597b3b5649af03f26
koukibuu3 <koukibuu3@gmail.com>
2022-02-24 23:10:17 +0900

    wip: add demo page

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App: React.VFC = () => {
-  return <h2 className="text-2xl">Hello from React!</h2>
+  return (
+    <div className="text-center font-mono text-xl my-24 mx-10">
+      <div>curse curtain curve custom customary customer cut</div>
+      <div>daily damage damp dance danger dare dark date dau</div>
+      <div>ghter day daylight dead deal dear death debt deca</div>
+      <div>y decide decision decisive declare decrease deed </div>
+      <div>deep defeat defend defendant defense degree delay</div>
+      <div>delicate delight deliver delivery demand departme</div>
+      <div>nt depend dependence dependent depth descend desc</div>
+      <div>ribe description desert deserve desire desk despa</div>
+      <div>ir destroy destruction destructive detail determi</div>
+      <div>ne develop devil dictionary die difference differ</div>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.body)
